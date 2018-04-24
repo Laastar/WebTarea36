ab8694e7fb74c061d7493559816bb037433016d9
laastar <machicaor@gmail.com>
2018-04-24 11:14:20 -0500

    Sitio Web

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('.materialboxed').materialbox();
+	$('.parallax').parallax();
+	$('.carousel').carousel();
+	$('.slider').slider();
+	$('.collapsible').collapsible();
+	$('.pushpin').pushpin();
+});
+
+$('.carousel.carousel-slider').carousel({
+	fullWidth: true
+});
